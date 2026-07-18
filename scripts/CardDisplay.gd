14a2f2e63c3aaf3c171dd66aa728763aac34542d
EnesBaytekin <enesbaytekin38@gmail.com>
2026-07-19 02:32:21 +0300

    feat: add states to cards

--- scripts/CardDisplay.gd
+++ scripts/CardDisplay.gd
@@ -1,11 +1,27 @@
 extends MarginContainer
 
+enum State { NORMAL, SELECTED, PICKABLE, DEFEATED }
+
 @export var card_data: Dictionary = {} : set = _set_card_data
+
+var _state: int = State.NORMAL
+var _base_y: float = 0.0
+var _tween: Tween = null
+var _target_y: float = INF
 
 @onready var user_pp: TextureRect = $Control/UserPP
 @onready var user_name: Label = $Control/UserName
 @onready var jam_count: Label = $Control/JamCount
 @onready var skill_labels: Array = [$Control/Skill1, $Control/Skill2, $Control/Skill3]
+@onready var select_highlight: ColorRect = $Control/SelectHighlight
+@onready var defeated_overlay: ColorRect = $Control/DefeatedOverlay
+
+
+func _ready():
+	_base_y = position.y
+	mouse_entered.connect(_on_hover)
+	mouse_exited.connect(_on_unhover)
+	_apply_state()
 
 
 func _set_card_data(value: Dictionary):
@@ -16,6 +32,85 @@ func _set_card_data(value: Dictionary):
 func display(data: Dictionary):
 	card_data = data
 	_refresh()
+
+
+func set_state(s: int):
+	_state = s
+	if is_inside_tree():
+		_apply_state()
+
+
+func get_state() -> int:
+	return _state
+
+
+func _smooth_move(target_y: float):
+	if is_equal_approx(position.y, target_y) or is_equal_approx(_target_y, target_y):
+		return
+	_target_y = target_y
+	if _tween:
+		_tween.kill()
+	_tween = create_tween().set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_SINE)
+	_tween.tween_property(self, "position:y", target_y, 0.1)
+	_tween.finished.connect(func(): _target_y = INF, CONNECT_ONE_SHOT)
+
+
+func _apply_state():
+	if not is_inside_tree():
+		return
+	match _state:
+		State.NORMAL:
+			mouse_filter = MOUSE_FILTER_IGNORE
+			_smooth_move(_base_y)
+			_remove_border()
+			select_highlight.hide()
+			defeated_overlay.hide()
+
+		State.SELECTED:
+			mouse_filter = MOUSE_FILTER_IGNORE
+			_smooth_move(_base_y - 6)
+			_add_border(Color(1, 1, 1), 2)
+			select_highlight.hide()
+			defeated_overlay.hide()
+
+		State.PICKABLE:
+			mouse_filter = MOUSE_FILTER_STOP
+			_smooth_move(_base_y)
+			_remove_border()
+			select_highlight.hide()
+			defeated_overlay.hide()
+
+		State.DEFEATED:
+			mouse_filter = MOUSE_FILTER_IGNORE
+			_smooth_move(_base_y)
+			_remove_border()
+			select_highlight.hide()
+			defeated_overlay.show()
+
+
+func _add_border(color: Color, width: float):
+	var sb = StyleBoxFlat.new()
+	sb.bg_color = Color.TRANSPARENT
+	sb.border_color = color
+	sb.set("border_width_all", width)
+	add_theme_stylebox_override("panel", sb)
+
+
+func _remove_border():
+	remove_theme_stylebox_override("panel")
+
+
+func _on_hover():
+	if _state == State.PICKABLE:
+		_smooth_move(_base_y - 6)
+		select_highlight.color = Color(1, 1, 1, 0.2)
+		select_highlight.show()
+
+
+func _on_unhover():
+	if _state == State.PICKABLE:
+		_smooth_move(_base_y)
+		select_highlight.hide()
 
 
 func _refresh():
@@ -41,3 +136,5 @@ func _refresh():
 			skill_labels[i].show()
 		else:
 			skill_labels[i].hide()
+
+	_apply_state()
--- scripts/TestCardGen.gd
+++ scripts/TestCardGen.gd
@@ -2,16 +2,20 @@ extends Control
 
 var card_scene: PackedScene = preload("res://scenes/Card.tscn")
 var cards_on_screen: Array = []
+var _last_clicked: Node = null
 
 @onready var container: Control = $CardContainer
 @onready var count_label: Label = $CountLabel
 @onready var spawn_btn: Button = $SpawnBtn
 @onready var clear_btn: Button = $ClearBtn
+@onready var state_btns: Array = [$NormalBtn, $SelectedBtn, $PickableBtn, $DefeatedBtn]
 
 
 func _ready():
 	spawn_btn.pressed.connect(_on_spawn)
 	clear_btn.pressed.connect(_on_clear)
+	for i in range(state_btns.size()):
+		state_btns[i].pressed.connect(_set_all_state.bind(i))
 	_update_count()
 
 
@@ -25,15 +29,22 @@ func _on_spawn():
 	var data = CardGenerator.generate_card()
 	var card = card_scene.instantiate()
 	card.display(data)
+	card.set_state(0)
 
-	# Place in a grid: row = floor(cards / 12), col = cards % 12
-	var col = cards_on_screen.size() % 12
-	var row = floori(cards_on_screen.size() / 12)
-	card.position = Vector2(col * 68 + 20, row * 84 + 20)
+	# 6 columns max, 96px wide card + 4px gap = 100px, fits in 640px viewport
+	var col = cards_on_screen.size() % 6
+	var row = floori(cards_on_screen.size() / 6)
+	card.position = Vector2(col * 100 + 20, row * 130 + 20)
 
 	container.add_child(card)
 	cards_on_screen.append(card)
 	_update_count()
+
+
+func _set_all_state(state: int):
+	for c in cards_on_screen:
+		if is_instance_valid(c):
+			c.set_state(state)
 
 
 func _on_clear():
